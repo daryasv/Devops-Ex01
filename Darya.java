c6f798627667fd05740450df026a1a8618741c1f
daryasv <svirskydarya@gmail.com>
2022-11-20 23:55:04 +0200

    change text in Darya file

--- Darya.java
+++ Darya.java
@@ -2,9 +2,9 @@ public class Darya
 {
     public static void main(String[] args) 
     {
-        for (int i=1;i<=4; i++) 
+        for (int i=1;i<=5; i++) 
         {
-            System.out.println("Darya :) ");
+            System.out.println("Eliya :)");
         }
     }
 }
